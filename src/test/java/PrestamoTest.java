fd940fb5d8a6842ec8ed375974bdb38147b05e11
jeliases <j.luisestrada98@gmail.com>
2026-03-30 21:05:58 -0500

    Se hace el test con cliente con Deuda || Se agrega el mockito y se limpia el pom

--- src/test/java/PrestamoTest.java
+++ src/test/java/PrestamoTest.java
@@ -1,6 +1,27 @@
-
+import Service.IConsultaService;
+import Service.PrestamoService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class PrestamoTest {
+
+    IConsultaService mockConsulta = mock(IConsultaService.class);
+    PrestamoService service = new PrestamoService(mockConsulta);
+
+    @Test
+    void testRechazoPorDeudas(){
+
+        when(mockConsulta.tieneDeudasPendientes("C100")).thenReturn(true);
+
+        String respuesta = service.registrarPrestamo("P001", 700,5000,1000,12, "C100");
+
+        assertEquals("Préstamo rechazado por deudas",respuesta);
+
+        verify(mockConsulta).tieneDeudasPendientes("C100");
+    }
+
 
 
 }
