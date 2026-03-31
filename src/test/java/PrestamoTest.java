649608e3eee785576bca9431a11364d7dfcbeca0
jeliases <j.luisestrada98@gmail.com>
2026-03-30 21:14:48 -0500

    test cuota excede

--- src/test/java/PrestamoTest.java
+++ src/test/java/PrestamoTest.java
@@ -10,18 +10,25 @@ public class PrestamoTest {
     IConsultaService mockConsulta = mock(IConsultaService.class);
     PrestamoService service = new PrestamoService(mockConsulta);
 
+    //**@Test
+    //**void testRechazoPorDeudas(){
+
+    //**when(mockConsulta.tieneDeudasPendientes("C100")).thenReturn(true);
+
+    //**String respuesta = service.registrarPrestamo("P001", 700,5000,1000,12, "C100");
+
+    //** assertEquals("Préstamo rechazado por deudas",respuesta);
+
+    //**verify(mockConsulta).tieneDeudasPendientes("C100");}
+
     @Test
-    void testRechazoPorDeudas(){
+    void testCuotaExcedida(){
+        when(mockConsulta.tieneDeudasPendientes("C200")).thenReturn(false);
+        when(mockConsulta.simulacionAprobada("P002")).thenReturn(true);
 
-        when(mockConsulta.tieneDeudasPendientes("C100")).thenReturn(true);
+        String respuesta = service.registrarPrestamo("P002", 750, 3000, 1500, 24, "C200");
+        System.out.println("Mensaje: " + respuesta);
+        assertEquals("Cuota excede el límite permitido", respuesta);
 
-        String respuesta = service.registrarPrestamo("P001", 700,5000,1000,12, "C100");
-
-        assertEquals("Préstamo rechazado por deudas",respuesta);
-
-        verify(mockConsulta).tieneDeudasPendientes("C100");
     }
-
-
-
 }
